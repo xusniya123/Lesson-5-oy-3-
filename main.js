cdbdc62e5489ab8bbb8e71a64d293dbea881ddcf
xusniya123 <xusniya12345678@gmail.com>
2024-04-12 16:14:37 +0500

    Lesson 5 moth (3)

---
+++ main.js
@@ -0,0 +1,22 @@
+// let maktab = {
+//     ustoz: 'Xurshida',
+//     xona: 'Jismoniy tarbiya' ,
+//     oquvchi: 'Nigora',
+//     milliygvardiya: 'Inspektor',
+//     oshxona: 'ovqat',
+// }
+
+// for (const key in maktab) {
+//  console.log(key + ":" + maktab[key]);
+// }
+
+let person = {
+    name: 'John',
+    age: 30 ,
+    city: 'New York'
+}
+
+
+for (const key in person) {
+  console.log(person[key]);
+}
